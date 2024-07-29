db8b33dd3593f2bd4d0bf2fc5bb761169d976615
Netanel Yaacobi <netanely3@gmail.com>
2024-07-29 13:09:12 +0300

    Replaces Answer1

--- Answer1.py
+++ Answer1.py
@@ -1,16 +1,16 @@
- def main():
+def main():
     numbers = []
     while True:
-        num = int(input("הכנס מספר שלם: "))
-        numbers.append(num)
+        num = int(input("Enter an integer: "))
         if 100 <= abs(num) <= 999:
             break
+        numbers.append(num)
     
     max_num = max(numbers)
     min_num = min(numbers)
     
-    print(f"המספר הגדול ביותר שנקלט: {max_num}")
-    print(f"המספר הקטן ביותר שנקלט: {min_num}")
+    print(f"The largest number entered: {max_num}")
+    print(f"The smallest number entered: {min_num}")
 
 if __name__ == "__main__":
     main()
